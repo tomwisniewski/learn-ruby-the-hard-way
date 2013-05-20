80e64c5522268a6e2077b03cfe22e9126a8490d6
Tom Wisniewski <tom.a.wisniewski@gmail.com>
2013-05-20 12:20:09 +0100

    Exercise 7

---
+++ ex7/ex7.rb
@@ -0,0 +1,31 @@
+# print string
+puts "Mary had a little lamb."
+
+# print string inserting 'snow'
+puts "Its fleece was white as %s" % 'snow'
+
+# print string
+puts "And everywhere that Mary went"
+
+# print '.' 10 times
+puts "." * 10
+
+# create a bunch of variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# print each variable consectutively without spaces
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
+ 
+puts
